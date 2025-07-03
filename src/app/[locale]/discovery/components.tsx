cc59eafdca7e956de20021e85f19a80f131539cc
hiusnef <hieu.pt03.office@gmail.com>
2025-07-03 16:41:04 +0700

    so tired

--- src/app/[locale]/discovery/components.tsx
+++ src/app/[locale]/discovery/components.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
-import {MapPin, Search, Star} from "lucide-react";
+import {Search} from "lucide-react";
 import {Input} from "@/components/ui/input";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import * as React from "react";
@@ -10,24 +10,19 @@ import {ChangeEventHandler, useEffect, useState} from "react";
 import {useTranslations} from "next-intl";
 import {cn} from "@/lib/utils";
 import {useRouter, useSearchParams} from "next/navigation";
-import Image from "next/image";
-import {Badge} from "@/components/ui/badge";
 import {Button} from "@/components/ui/button";
-import {Link} from "@/i18n/navigation";
 import {Cuisine, getCuisines} from "@/services/cuisine.service";
-import {ShortDishProps} from "@/services/dish.service";
+
 
 interface FilterProps {
     search?: string;
     cuisine?: string;
-    priceRange?: [number, number];
-    maxPrice?: number
 }
 
 export const DiscoveryFilter = () => {
     const [cuisines, setCuisines] = useState<Cuisine[]>([]);
     useEffect(() => {
-        getCuisines().then(fetchCuisines => setCuisines(fetchCuisines));
+        getCuisines().then(fetchCuisines => setCuisines(fetchCuisines || []));
     }, []);
 
     const t = useTranslations("discovery.filter");
@@ -44,10 +39,11 @@ export const DiscoveryFilter = () => {
     });
 
     const onClickSearch = () => {
-        const params: Record<string, any> = {};
+        const params: Record<string, string> = {};
         if (filters.search) params.search = filters.search;
-        if (filters.cuisine && filters.cuisine !== "all") params.cuisine = filters.cuisine;
-        router.replace(`/discovery?${new URLSearchParams(params).toString()}`);
+        if (filters.cuisine) params.cuisine = filters.cuisine;
+        const queryString = new URLSearchParams(params).toString();
+        router.push(queryString ? `/discovery?${queryString}` : '/discovery');
     }
 
     return (
@@ -67,7 +63,7 @@ export const DiscoveryFilter = () => {
                     <CuisineFilter
                         cuisines={cuisines}
                         selected={filters.cuisine}
-                        onValueChange={(value) => setFilters({...filters, cuisine: value || "all"})}
+                        onValueChange={(value) => setFilters({...filters, cuisine: value || '0'})}
                     />
                     <Button
                         className={"bg-orange-500 hover:bg-orange-600 rounded-xl"}
@@ -118,49 +114,11 @@ function CuisineFilter({cuisines, selected, onValueChange, className}: Readonly<
                     <SelectValue placeholder={t("cuisines")}/>
                 </SelectTrigger>
                 <SelectContent>
-                    {cuisines.map((cuisine) => (
+                    {cuisines?.map((cuisine) => (
                         <SelectItem key={cuisine.id} value={`${cuisine.id}`}>{cuisine.name}</SelectItem>
                     ))}
                 </SelectContent>
             </Select>
         </div>
     );
-}
-
-export const FoodCard = ({dish}:{dish: ShortDishProps}) => {
-    console.log(dish)
-    return (
-        <Link key={dish.id} href={`/restaurant/1/${dish.id}`}>
-            <Card className="border-gray-100 hover:shadow-lg transition-shadow cursor-pointer">
-                <div className="relative">
-                    <Image
-                        // src={dish.images?.[0] ?? "/logo.svg"}
-                        src={"/logo.svg"}
-                        alt={dish.name}
-                        className="w-full h-48 object-cover rounded-t-lg"
-                        width={32} height={32}
-                    />
-                </div>
-
-                <CardContent className="p-4">
-                    <span className={"flex items-center justify-between"}>
-                        <h3 className="font-semibold text-lg mb-1">{dish.name}</h3>
-                        <Badge variant="outline">{dish.cuisine}</Badge>
-                    </span>
-                    <h4>{dish.price}</h4>
-
-                    <div className="flex items-center justify-between mb-3">
-                        <div className="flex items-center space-x-1">
-                            <MapPin className="w-4 h-4 text-gray-400"/>
-                            <span className="text-sm text-gray-600">{dish.restaurant}</span>
-                        </div>
-                        <div className="flex items-center space-x-1">
-                            <Star className="w-4 h-4 text-yellow-400 fill-yellow-400"/>
-                            <span className="text-sm font-medium">{dish.rating.toFixed(1)}</span>
-                        </div>
-                    </div>
-                </CardContent>
-            </Card>
-        </Link>
-    )
 }
